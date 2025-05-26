e4a80f854fe47d4cdcd65af55ce6c9928302725e
yoogun12 <yoogun1313@gmail.com>
2025-05-26 13:38:24 +0900

    [Feat] 계급장 버그 수정 완료

--- Assets/Scripts/Game_RankMerge/GameManager.cs
+++ Assets/Scripts/Game_RankMerge/GameManager.cs
@@ -72,6 +72,8 @@ public class GameManager : MonoBehaviour
         DraggableRank rank = rankObj.AddComponent<DraggableRank>();
         rank.SetRankLevel(level);
 
+        cell.SetRank(rank);
+
         return rank;
 
     }
